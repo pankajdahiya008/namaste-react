c36ff7ccc4f9f9112cb5c38d6fe14273d59cc5c7
Pankaj Dahiya <pankaj.id.008@gmail.com>
2024-12-24 20:24:40 +0530

    changed home page api link to point to swiggy proxy

--- src/components/Body.js
+++ src/components/Body.js
@@ -22,7 +22,7 @@ const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+      "https://swiggy-proxy.onrender.com/api/restaurants?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
     );
 
     const json = await data.json();
